9f4ef08dedb651a10444178f5d0e2d8ab86f5449
Faizan Majgaonkar <121805322+faizan-t09@users.noreply.github.com>
2023-03-24 15:19:36 +0530

    added getter setter and validate to models

--- models/roleModel.ts
+++ models/roleModel.ts
@@ -1,5 +1,6 @@
 import { Sequelize, DataTypes } from "sequelize";
 import seqConn from "../dbConn";
+import User from "./userModel";
 
 const Role = seqConn.define(
   "Role",
@@ -14,22 +15,43 @@ const Role = seqConn.define(
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate:{
+        notEmpty:true,
+        isAlpha:true,
+        len:[3,20]
+      }
     },
     createdBy: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     updatedBy: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     deletedBy: {
       type: DataTypes.INTEGER,
       allowNull: true,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     deletedAt: {
       type: DataTypes.DATE,
       allowNull: true,
+      validate:{
+        notEmpty:true,
+        isDate:true,
+      }
     },
   },
   {
--- models/userModel.ts
+++ models/userModel.ts
@@ -1,5 +1,9 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize, DataTypes, BelongsTo } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 import seqConn from "../dbConn";
+import Role from "./roleModel";
+const encryptpwd = require("encrypt-with-password");
 
 const User = seqConn.define(
   "User",
@@ -14,36 +18,80 @@ const User = seqConn.define(
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      get() {
+        return this.getDataValue("username").toUpperCase();
+      },
+      validate:{
+        notEmpty:true,
+        isAlpha:true,
+        len:[3,20],
+      }
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      set(value) {
+        this.setDataValue(
+          "password",
+          encryptpwd.encrypt(value, process.env.ENCRYPTION_KEY)
+        );
+      },
+      validate:{
+        notEmpty:true,
+        isAlphanumeric:true,
+        len:[8,20],
+      }
     },
     roleId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     createdBy: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     updatedBy: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     deletedBy: {
       type: DataTypes.INTEGER,
       allowNull: true,
+      validate:{
+        notEmpty:true,
+        isNumeric:true,
+      }
     },
     deletedAt: {
       type: DataTypes.DATE,
       allowNull: true,
+      validate:{
+        notEmpty:true,
+        isDate:true,
+      }
     },
   },
   {
     tableName: "users",
   }
 );
+
+User.belongsTo(Role, {
+  foreignKey: "roleId",
+  targetKey: "id",
+});
 
 export default User;
